1ac63b09b3af83869ede815c153828446af4e9c7
bong-coding <qhdals1611@naver.com>
2024-11-06 22:11:08 +0900

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+import auth from "./modules/auth";
+import movies from "./modules/movies";
+import wishlist from "./modules/wishlist";
+
+export default createStore({
+  modules: {
+    auth,
+    movies,
+    wishlist,
+  },
+});
